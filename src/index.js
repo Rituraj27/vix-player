dbc5e26a0edf3db999b532f0f706be5ec9e6cde7
Rituraj27 <ri2rajhao1@gmail.com>
2025-09-01 14:52:34 +0530

    Errors and api response Boilerplate created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import connectDb from './db/index.js';
-
-const app = express();
+import { app } from './app.js';
 
 dotenv.config({
   path: './env',
 });
 console.log(process.env.PORT);
 
-connectDb();
+connectDb()
+  .then(() => {
+    console.log('Server connected successfully');
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Database connection failed', error);
+  });
 
 /*
 import express from 'express';
